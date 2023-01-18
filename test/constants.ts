d42f5a84f9a82f6476e18a614b574d86e7dc2684
sam <sam@functionless.org>
2023-01-18 20:43:31 +1100

    feat: set up repeatable test and improve error (#11)

---
+++ test/constants.ts
@@ -0,0 +1,7 @@
+export const TableName = "itty-test-table";
+
+export const SsmParameterName = "itty-parameter-name";
+
+export const SsmParameterValue = "itty-parameter-value";
+
+process.env.AWS_REGION = "us-west-2";
