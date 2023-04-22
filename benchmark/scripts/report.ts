13f93b890889273663fba1a8da72b15ff3d98996
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2023-04-22 15:16:09 +0200

    feat: generate markdown report

--- benchmark/scripts/report.ts
+++ benchmark/scripts/report.ts
@@ -25,7 +25,6 @@ const createChart = async (props: {
   min: number;
   offsetData: number[];
   rightData: number[];
-  title: string;
   unit: string;
   datalabels: string[];
 }) => {
@@ -38,7 +37,6 @@ const createChart = async (props: {
     indexAxis: "y",
     scales: {
       x: {
-        // type: "logarithmic",
         stacked: true,
         max: props.max,
         min: props.min,
@@ -49,12 +47,18 @@ const createChart = async (props: {
       },
       y: {
         stacked: true,
+        ticks: {
+          crossAlign: "far",
+        },
       },
     },
     plugins: {
       title: {
-        display: true,
-        text: props.title,
+        display: false,
+        align: "start",
+        font: {
+          size: 20,
+        },
       },
       legend: {
         display: false,
@@ -111,7 +115,9 @@ const createChart = async (props: {
   const height = 600;
   const backgroundColour = "white";
   const chartCallback: ChartCallback = (ChartJS) => {
-    ChartJS.defaults.responsive = true;
+    ChartJS.defaults.color = "rgba(31, 35, 40)";
+    ChartJS.defaults.font.size = 16;
+    ChartJS.defaults.responsive = false;
     ChartJS.defaults.maintainAspectRatio = false;
   };
   const chartJSNodeCanvas = new ChartJSNodeCanvas({
@@ -130,6 +136,71 @@ const createChart = async (props: {
   });
   await fsPromises.writeFile(filePath, buffer, "base64");
 };
+
+function generateCharts(
+  datasetType: string,
+  datasets: Record<string, Record<string, any>>,
+  gitBranch: string
+) {
+  for (const key in datasets) {
+    const labels: string[] = [];
+    const datalabels: string[] = [];
+    const backgroundColors: string[] = [];
+    const borderColors: string[] = [];
+    const offsetData: number[] = [];
+    const leftData: number[] = [];
+    const rightData: number[] = [];
+    let max = 0;
+    let min = Infinity;
+    console.log(`  - ${datasetType} : ${key}`);
+    const dataset = Object.fromEntries(
+      Object.entries(datasets[key as keyof typeof datasets]).sort(
+        (a, b) => a[1].order - b[1].order
+      )
+    );
+    for (const key in dataset) {
+      if (dataset[key].data) {
+        const { label, mean, standardDeviation } = dataset[key];
+        labels.push(label);
+        datalabels.push(`${mean}±${standardDeviation}`);
+        backgroundColors.push(
+          CONFIG.functions.reduce(
+            (prev, curr) =>
+              curr.functionName === key ? curr.chart.backgroundColor : prev,
+            ""
+          )
+        );
+        borderColors.push(
+          CONFIG.functions.reduce(
+            (prev, curr) =>
+              curr.functionName === key ? curr.chart.borderColor : prev,
+            ""
+          )
+        );
+        offsetData.push(mean - standardDeviation);
+        leftData.push(standardDeviation);
+        rightData.push(standardDeviation);
+        max = Math.max(max, mean + standardDeviation);
+        min = Math.min(min, mean - standardDeviation);
+      }
+    }
+    createChart({
+      backgroundColors,
+      borderColors,
+      datalabels,
+      datasetName: key,
+      datasetType,
+      gitBranch,
+      labels,
+      leftData,
+      max: Math.ceil(max / 10) * 10,
+      min: Math.floor(min / 5) * 5,
+      offsetData,
+      rightData,
+      unit: key === "maxMemory" ? "MB" : "ms",
+    });
+  }
+}
 
 async function main() {
   const start = performance.now();
@@ -155,6 +226,11 @@ async function main() {
     CONFIG.logs.outputFolder,
     `${gitBranch}/consolidated.json`
   );
+  const reportFilePath = path.join(
+    __dirname,
+    CONFIG.logs.outputFolder,
+    `${gitBranch}/README.md`
+  );
   console.log(
     `Generate report for branch '${gitBranch}' ${gitTag ? `(${gitTag})` : ""}`
   );
@@ -173,132 +249,60 @@ async function main() {
 
   console.log(`- Read raw data`);
   let {
+    time,
     datasets: { coldStarts, warmStarts },
   }: IResult = JSON.parse(
     await fsPromises.readFile(consolidatedFilePath, "utf8")
   );
 
   console.log(`- Generate the charts for`);
-  for (const coldStartsKey in coldStarts) {
-    const labels: string[] = [];
-    const datalabels: string[] = [];
-    const backgroundColors: string[] = [];
-    const borderColors: string[] = [];
-    const offsetData: number[] = [];
-    const leftData: number[] = [];
-    const rightData: number[] = [];
-    let max = 0;
-    let min = Infinity;
-    console.log(`  - Cold starts : ${coldStartsKey}`);
-    const dataset = Object.fromEntries(
-      Object.entries(coldStarts[coldStartsKey as keyof typeof coldStarts]).sort(
-        (a, b) => a[1].order - b[1].order
-      )
-    );
-    for (const key in dataset) {
-      if (dataset[key].data) {
-        const { label, mean, standardDeviation } = dataset[key];
-        labels.push(label);
-        datalabels.push(`${mean}±${standardDeviation}`);
-        backgroundColors.push(
-          CONFIG.functions.reduce(
-            (prev, curr) =>
-              curr.functionName === key ? curr.chart.backgroundColor : prev,
-            ""
-          )
-        );
-        borderColors.push(
-          CONFIG.functions.reduce(
-            (prev, curr) =>
-              curr.functionName === key ? curr.chart.borderColor : prev,
-            ""
-          )
-        );
-        offsetData.push(mean - standardDeviation);
-        leftData.push(standardDeviation);
-        rightData.push(standardDeviation);
-        max = Math.max(max, mean + standardDeviation);
-        min = Math.min(min, mean - standardDeviation);
-      }
-    }
-    createChart({
-      backgroundColors,
-      borderColors,
-      datalabels,
-      datasetName: coldStartsKey,
-      datasetType: "coldStarts",
-      gitBranch,
-      labels,
-      leftData,
-      max: Math.ceil(max / 10) * 10,
-      min: Math.floor(min / 5) * 5,
-      offsetData,
-      rightData,
-      title: dataset.title,
-      unit: coldStartsKey === "maxMemory" ? "MB" : "ms",
-    });
-  }
+  generateCharts("coldStarts", coldStarts, gitBranch);
+  generateCharts("warmStarts", warmStarts, gitBranch);
 
-  for (const warmStartsKey in warmStarts) {
-    const labels: string[] = [];
-    const datalabels: string[] = [];
-    const backgroundColors: string[] = [];
-    const borderColors: string[] = [];
-    const offsetData: number[] = [];
-    const leftData: number[] = [];
-    const rightData: number[] = [];
-    let max = 0;
-    let min = Infinity;
-    console.log(`  - Warm starts : ${warmStartsKey}`);
-    const dataset = Object.fromEntries(
-      Object.entries(warmStarts[warmStartsKey as keyof typeof warmStarts]).sort(
-        (a, b) => a[1].order - b[1].order
-      )
-    );
-    for (const key in dataset) {
-      if (dataset[key].data) {
-        const { label, mean, standardDeviation } = dataset[key];
-        labels.push(label);
-        datalabels.push(`${mean}±${standardDeviation}`);
-        backgroundColors.push(
-          CONFIG.functions.reduce(
-            (prev, curr) =>
-              curr.functionName === key ? curr.chart.backgroundColor : prev,
-            ""
-          )
-        );
-        borderColors.push(
-          CONFIG.functions.reduce(
-            (prev, curr) =>
-              curr.functionName === key ? curr.chart.borderColor : prev,
-            ""
-          )
-        );
-        offsetData.push(mean - standardDeviation);
-        leftData.push(standardDeviation);
-        rightData.push(standardDeviation);
-        max = Math.max(max, mean + standardDeviation);
-        min = Math.min(min, mean - standardDeviation);
-      }
-    }
-    createChart({
-      backgroundColors,
-      borderColors,
-      datalabels,
-      datasetName: warmStartsKey,
-      datasetType: "warmStarts",
-      gitBranch,
-      labels,
-      leftData,
-      max: Math.ceil(max / 5) * 5,
-      min: Math.floor(min / 5) * 5,
-      offsetData,
-      rightData,
-      title: dataset.title,
-      unit: warmStartsKey === "maxMemory" ? "MB" : "ms",
-    });
-  }
+  console.log(`\nGenerate README.md`);
+  const content = `
+# Benchmark report
 
+- Branch: ${gitBranch}${gitTag ? ` (v${gitTag})` : ""}
+- Date: ${new Date(time.end).toLocaleString()}
+
+## Cold starts
+
+### Cold starts total duration
+![coldStarts-totalDuration](./coldStarts-totalDuration.png)
+
+### Cold starts init duration
+![coldStarts-initDuration](./coldStarts-initDuration.png)
+
+### Cold starts duration
+![coldStarts-duration](./coldStarts-duration.png)
+
+### Cold starts API call latency
+![coldStarts-apiCallLatency](./coldStarts-apiCallLatency.png)
+
+### Cold starts HTTP Request latency
+![coldStarts-httpRequestLatency](./coldStarts-httpRequestLatency.png)
+
+### Cold starts max memory
+![coldStarts-maxMemory](./coldStarts-maxMemory.png)
+
+## Warm starts
+
+### Warm starts duration
+![warmStarts-duration](./warmStarts-duration.png)
+
+### Warm starts API call latency
+![warmStarts-apiCallLatency](./warmStarts-apiCallLatency.png)
+
+
+### Warm starts HTTP request latency
+![warmStarts-httpRequestLatency](./warmStarts-httpRequestLatency.png)
+
+### Warm starts max memory
+![warmStarts-maxMemory](./warmStarts-maxMemory.png)
+
+`;
+  await fsPromises.writeFile(reportFilePath, content, "utf-8");
   const duration = performance.now() - start;
   console.log(`\nDone in ${round(duration)}ms.`);
 }
