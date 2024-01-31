b9d2180c2fab459fb1759d38e37329deaedec90e
Cristian Pallarés <cristianp@wing.cloud>
2024-01-31 10:58:23 +0100

    wip

--- scripts/download-smithy-models.sh
+++ scripts/download-smithy-models.sh
@@ -1,5 +1,6 @@
-git clone git@github.com:aws/aws-sdk-js-v3.git --depth=1
+git clone https://github.com/aws/aws-sdk-js-v3.git --depth=1
 
+rm -rf aws-models
 mv aws-sdk-js-v3/codegen/sdk-codegen/aws-models .
 
 rm -rf aws-sdk-js-v3
