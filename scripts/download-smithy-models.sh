29d9bba8a36fd69b32f6e909e3e0b839acd37453
Cristian Pallarés <cristianp@wing.cloud>
2024-03-06 10:56:25 +0100

    use git protocol again

--- scripts/download-smithy-models.sh
+++ scripts/download-smithy-models.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/aws/aws-sdk-js-v3.git --depth=1
+git clone git@github.com:aws/aws-sdk-js-v3.git --depth=1
 
 rm -rf aws-models
 mv aws-sdk-js-v3/codegen/sdk-codegen/aws-models .
